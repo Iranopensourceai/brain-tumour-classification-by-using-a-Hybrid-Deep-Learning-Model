76f31c08ed51b5907f6bda00cd15a43f7012df7f
Siamand Avestan <siamand.avestan@gmail.com>
2023-01-30 01:18:09 +0330

    import other module in main

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 '''
 Creating a data frame and saving the path and label of each image in the data frame
 '''
-import os 
-import pandas as pd
-import numpy as np
-from tqdm import tqdm 
-import cv2
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from generate_datafram import generate_datafram
+from read_data import Read_flow_from_dataframe
 BATCH_SIZE=32
-IMG_HEIGHT = 224
-IMG_WIDTH = 224
-CHANNELS = 3
 path = '/kaggle/input/brain-tumor-classification-mri/'
-class_label= ["no_tumor", "pituitary_tumor", "meningioma_tumor", "glioma_tumor"]
-
 train_df,test_df=generate_datafram(path)
-train_set,test_set = Read_flow_from_dataframe(train_df,test_df)
+train_set,test_set = Read_flow_from_dataframe(train_df,test_df,BATCH_SIZE)
