bc3f84526e3ba899191f226755e8ec60df13af03
Siamand Avestan <siamand.avestan@gmail.com>
2023-01-31 15:56:09 +0330

    added preprocessing function

---
+++ dataLoader/preprocessing.py
@@ -0,0 +1,15 @@
+import cv2
+import torch
+### image preprocessing function
+def prepare_image(path):
+    # import image
+    image = cv2.imread(path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  
+    # resize and color
+    image = cv2.resize(image, (int(image_size), int(image_size)))
+    image = image.astype("float32") / 255.0
+    # convert to tensor    
+    image = torch.tensor(image)
+    image = image.permute(2, 1, 0)
+    return image
+
