bae2843e495e6b424def370bbc6f16c23c6e7bda
Siamand Avestan <siamand.avestan@gmail.com>
2023-01-31 16:17:40 +0330

    create train adn test dataset and added dataloader

--- dataLoader/read_data.py
+++ dataLoader/read_data.py
@@ -1,28 +1,36 @@
 '''
  1.2 Use flow_from_dataframe for read data
 '''
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import torch
+from torchvision import transforms
+from TomurDataset import TomurTrainData ,TomurTestData
+
 class_label= ["no_tumor", "pituitary_tumor", "meningioma_tumor", "glioma_tumor"]
-def Read_flow_from_dataframe(train_df,test_df,BATCH_SIZE):
-    train_datagen= ImageDataGenerator()
-    train_set= train_datagen.flow_from_dataframe(train_df,
-                                               x_col='images',
-                                               y_col='labels',
-                                               target_size=(224, 224),
-                                               classes=class_label,
-                                               shuffle=True,
-                                               batch_size=BATCH_SIZE,
-                                               class_mode='categorical')
+# train transformations
+train_trans = transforms.Compose([transforms.ToPILImage(),
+                                  transforms.ToTensor(),
+                                  transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])])
 
-    # test_datagen= ImageDataGenerator(rescale = 1./255)
-    test_datagen= ImageDataGenerator()
+# validation transformations
+valid_trans = transforms.Compose([transforms.ToPILImage(),
+                                  transforms.ToTensor(),
+                                  transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])])
+def Data_Loader(train_df,test_df,BATCH_SIZE,path):
 
-    test_set= test_datagen.flow_from_dataframe(test_df,
-                                               x_col='images',
-                                               y_col='labels',
-                                               target_size=(224, 224),
-                                               classes=class_label,
-                                               shuffle=True,
-                                               batch_size=BATCH_SIZE,
-                                               class_mode='categorical')
-    return train_set,test_set
+    train_dataset = TomurTrainData(data      = train_df, 
+                             directory = path,
+                             transform = train_trans)
+    valid_dataset = TomurTestData(data       = test_df, 
+                                directory  = path,
+                                transform  = valid_trans)
+
+    # create data loaders
+    train_loader = torch.utils.data.DataLoader(train_dataset, 
+                                            batch_size  = BATCH_SIZE, 
+                                            shuffle     = True, 
+                                            num_workers = 4)
+    valid_loader = torch.utils.data.DataLoader(valid_dataset, 
+                                            batch_size  = BATCH_SIZE, 
+                                            shuffle     = True, 
+                                            num_workers = 4)
+    return train_loader,valid_loader
