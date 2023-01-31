dcd800432b6d9b8950fd526a38cda13b0ebf9b25
Siamand Avestan <siamand.avestan@gmail.com>
2023-01-31 17:12:15 +0330

    change folder name

---
+++ code/TomurDataset.py
@@ -0,0 +1,49 @@
+import os
+import torch
+from torch.utils.data import Dataset
+from preprocessing import prepare_image
+
+
+# dataset class: train
+class TomurTrainData(Dataset):
+    
+    # initialize
+    def __init__(self, data, directory, transform = None):
+        self.data      = data
+        self.directory = directory
+        self.transform = transform
+        
+    # length
+    def __len__(self):
+        return len(self.data)
+    
+    # get items    
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.directory, self.data.loc[idx, 'id_code'] )
+        image    = prepare_image(img_name)
+        image    = self.transform(image)
+        label    = torch.tensor(self.data.loc[idx, 'diagnosis'])
+        return {'image': image, 'label': label}
+    
+    
+# dataset class: test
+class TomurTestData(Dataset):
+    
+    # initialize
+    def __init__(self, data, directory, transform = None):
+        self.data      = data
+        self.directory = directory
+        self.transform = transform
+        
+    # length
+    def __len__(self):
+        return len(self.data)
+    
+    # get items    
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.directory, self.data.loc[idx, 'id_code'] )
+        image    = prepare_image(img_name)
+        image    = self.transform(image)
+        label    = torch.tensor(self.data.loc[idx, 'diagnosis'])
+        return {'image': image, 'label': label}
+
---
+++ code/generate_datafram.py
@@ -0,0 +1,24 @@
+import os
+import pandas as pd
+'''
+ 1.1 add image path and labels in datafram
+'''
+def generate_datafram(path):
+    train_labels = []
+    train_images = []
+
+    test_labels = []
+    test_images = []
+
+    for sub_dir,dir,files in os.walk(path):
+        for file in files:
+            img_path = os.path.join(sub_dir,file)
+            if img_path.split('/')[-3] == 'Training':
+                train_images.append(img_path)
+                train_labels.append(img_path.split('/')[-2])
+            elif img_path.split('/')[-3] == 'Testing':
+                train_images.append(img_path)
+                train_labels.append(img_path.split('/')[-2])
+    df = pd.DataFrame(list(zip(train_images,train_labels)),columns = ['images','labels'])
+    #test_df = pd.DataFrame(list(zip(test_images,test_labels)),columns = ['images','labels'])
+    return df
