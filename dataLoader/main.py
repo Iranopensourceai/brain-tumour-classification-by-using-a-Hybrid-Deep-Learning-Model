a70ce3a458119b6c2403efa07011a320311a5c1d
Siamand Avestan <siamand.avestan@gmail.com>
2023-01-31 16:34:37 +0330

    import all module to main and used

--- dataLoader/main.py
+++ dataLoader/main.py
@@ -7,11 +7,14 @@ from generate_datafram import generate_datafram
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from read_data import Data_Loader
-
-
+import torch.nn as nn
+from Model import init_model
+from torch import optim
+from train import train
+n_epochs =30
 BATCH_SIZE=32
 path = '/kaggle/input/brain-tumor-classification-mri/'
-
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 labelencoder = LabelEncoder()
 df=generate_datafram(path)
 df['diagnosis'] = labelencoder.fit_transform(df['diagnosis'])
@@ -21,3 +24,10 @@ test_df = pd.DataFrame(list(zip(X_test,Y_test)),columns = ['id_code','diagnosis'
 
 # create datasets
 train_loader,valid_loader = Data_Loader(train_df,test_df,BATCH_SIZE,path)
+
+model = init_model().to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
+
+train(train_loader,valid_loader,model,n_epochs,device,optimizer,criterion)
