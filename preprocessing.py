110ae48a9fbf1a6906943547e09a2906222d6541
Sadaf Sattarzadeh <121829224+sadafsattarzadeh@users.noreply.github.com>
2023-02-17 21:26:52 +0330

    Add files via upload

---
+++ preprocessing.py
@@ -0,0 +1,22 @@
+import cv2
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+IMG_HEIGHT = 224
+IMG_WIDTH = 224
+
+def preprocess_image(image_path):
+    img = cv2.imread(image_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+    return img
+
+
+def data_augmentation():
+   model=keras.Sequential(
+    [
+      layers.RandomFlip("horizontal"),
+      layers.RandomRotation(0.1),
+      layers.RandomZoom(0.2),
+    ])
+   return model
